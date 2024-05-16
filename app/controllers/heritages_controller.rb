0b8bea19ff96e9eeb7a8f39b998404530a7d26d5
hataken <bb52119638@gmail.com>
2024-05-16 15:27:06 +0900

    世界遺産への口コミ投稿機能

---
+++ app/controllers/heritages_controller.rb
@@ -0,0 +1,7 @@
+class HeritagesController < ApplicationController
+  def show
+    @heritage = Heritage.find(params[:id])
+    @reviews = @heritage.reviews
+    @review = @heritage.reviews.build
+  end
+end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+  def create
+    heritage = Heritage.find(params[:heritage_id])
+    review = heritage.reviews.build(review_params)
+    review.user = current_user
+    review.review_image.attach(params[:review][:review_image])
+    if review.save
+      flash[:success] = "口コミが投稿されました"
+      redirect_to root_url
+    else
+      render 'heritages/show', status: :unprocessable_entity
+    end
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:content, :review_image)
+    end
+end
