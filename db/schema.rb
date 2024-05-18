c25ac8cec1494df664a3a909b12125835724c1a8
hataken <bb52119638@gmail.com>
2024-05-18 14:18:31 +0900

    とりあえずparent_idカラムだけ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_18_031125) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_18_051344) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_18_031125) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "parent_id"
     t.index ["heritage_id"], name: "index_reviews_on_heritage_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
