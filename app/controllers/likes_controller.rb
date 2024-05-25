7dd101b55f21afcd3107df671d3e94bfced25eb9
hataken <bb52119638@gmail.com>
2024-05-25 11:55:38 +0900

    非同期いいね機能を途中まで実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,24 +2,20 @@ class LikesController < ApplicationController
   before_action :logged_in_user
 
   def create
-    review = Review.find(params[:review_id])
-    current_user.like_review(review)
-    heritage = Heritage.find(review.heritage_id)
-    redirect_to heritage
-    # respond_to do |format|
-    #   format.html { redirect_to current_user }
-    #   format.turbo_stream
-    # end
+    @review = Review.find(params[:review_id])
+    current_user.like_review(@review)
+    heritage = Heritage.find(@review.heritage_id)
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
-    review = Review.find(params[:review_id])
-    current_user.unlike_review(review)
-    heritage = Heritage.find(review.heritage_id)
-    redirect_to heritage
-    # respond_to do |format|
-    #   format.html { redirect_to current_user, status: :see_other }
-    #   format.turbo_stream
-    # end
+    @review = Review.find(params[:review_id])
+    current_user.unlike_review(@review)
+    heritage = Heritage.find(@review.heritage_id)
+    respond_to do |format|
+      format.js
+    end
   end
 end
