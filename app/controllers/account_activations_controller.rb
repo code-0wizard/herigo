b61bcfd80a8f5600cd68253f46985f51d9e746f7
hataken <bb52119638@gmail.com>
2024-06-13 16:09:36 +0900

    アカウント有効化の処理を修正

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,11 +5,9 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = "Account activated!"
-      redirect_to user
+      render 'success', status: :ok
     else
-      flash[:danger] = "Invalid activation link"
-      redirect_to root_url
+      render 'failed', status: :unprocessable_entity
     end
   end
 end
