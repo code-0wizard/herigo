ce9d72fa056b0008366d25b4f63188c1f01909ba
hataken <bb52119638@gmail.com>
2024-06-15 16:17:38 +0900

    魅力登録、詳細ページ完成

--- app/controllers/heritage_charms_controller.rb
+++ app/controllers/heritage_charms_controller.rb
@@ -3,10 +3,28 @@ class HeritageCharmsController < ApplicationController
   end
 
   def new
-    @heritage_charm = HeritageCharm.new
+    @heritage_charms = Array.new(4) do
+      heritage_charm = HeritageCharm.new
+      heritage_charm.main_image.build
+      heritage_charm.sub_image.build
+      heritage_charm
+    end
   end
 
   def create
+    debugger
+    heritage = Heritage.find(params[:heritage_id])
+    @heritage_charms = params[:heritage_charms].map do |heritage_charm_param|
+      heritage_charm = heritage.heritage_charms.build(heritage_charm_param.permit(:title, :description, :order))
+      heritage_charm.main_image.attach(heritage_charm_param[:main_image])
+      heritage_charm.sub_image.attach(heritage_charm_param[:sub_image])
+      heritage_charm
+    end
+    if @heritage_charms.all?(&:save)
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def edit
