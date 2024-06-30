a5940ee9d1a03fec105d8b6ad74abd2a06823a65
hataken <bb52119638@gmail.com>
2024-06-30 14:48:46 +0900

    アカウント有効化メールの単体試験実装

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  # describe 'account_activation' do
-  #   let(:mail) { UserMailer.account_activation }
+  describe 'アカウント有効化' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:mail) { UserMailer.account_activation(user) }
 
-  #   it 'renders the headers' do
-  #     expect(mail.subject).to eq('Account activation')
-  #     expect(mail.to).to eq(['to@example.org'])
-  #     expect(mail.from).to eq(['from@example.com'])
-  #   end
+    it "「アカウント有効化」というタイトルで送信されること" do
+      expect(mail.subject).to eq('アカウント有効化')
+    end
 
-  #   it 'renders the body' do
-  #     expect(mail.body.encoded).to match('Hi')
-  #   end
-  # end
+    it '送信先がユーザーのメールアドレス宛であること' do
+      expect(mail.to).to eq([user.email])
+    end
 
-  # describe 'password_reset' do
-  #   let(:mail) { UserMailer.password_reset }
+    it '送信元が「from@example.com」であること' do
+      expect(mail.from).to eq(['from@example.com'])
+    end
 
-  #   it 'renders the headers' do
-  #     expect(mail.subject).to eq('Password reset')
-  #     expect(mail.to).to eq(['to@example.org'])
-  #     expect(mail.from).to eq(['from@example.com'])
-  #   end
+    it 'メール本文にユーザ名が表示されていること' do
+      expect(mail.html_part.body.to_s).to include(user.name)
+    end
 
-  #   it 'renders the body' do
-  #     expect(mail.body.encoded).to match('Hi')
-  #   end
-  # end
+    it 'メール本文にユーザのactivation_tokenが表示されていること' do
+      user.activation_token = User.new_token
+      activation_url = edit_account_activation_url(user.activation_token, email: user.email)
+      expect(mail.html_part.body.to_s).to include(activation_url)
+    end
+
+  end
 end
