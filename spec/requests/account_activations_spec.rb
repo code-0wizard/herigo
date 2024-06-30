9b2bec6ebc11bfcf87d9fb238b127bd36558dadb
hataken <bb52119638@gmail.com>
2024-06-30 17:31:55 +0900

    アカウント有効化の結合試験実装

--- spec/requests/account_activations_spec.rb
+++ spec/requests/account_activations_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
+include SessionsHelper
 
 RSpec.describe 'AccountActivations', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe '/account_activations/{id}/edit' do
+    before do
+      post users_path, params: { user: { name: 'テストユーザー',
+                                         email: 'user@example.com',
+                                         password: 'password123',
+                                         password_confirmation: 'password123' } }
+      @user = controller.instance_variable_get('@user')
+    end
+
+    context 'トークンとemailが有効な場合' do
+      it 'activateされること' do
+        get edit_account_activation_path(@user.activation_token, email: @user.email)
+        @user.reload
+        expect(@user).to be_activated
+      end
+ 
+      it 'ログイン状態になること' do
+        get edit_account_activation_path(@user.activation_token, email: @user.email)
+        expect(logged_in?).to be_truthy
+      end
+    end
   end
 end
