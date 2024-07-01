aa62fb4683264f05d9e7f24c31a390506587cdc9
hataken <bb52119638@gmail.com>
2024-07-01 18:41:50 +0900

    TEST｜ログイン試験(途中①)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     password_confirmation { 'password1' }
     activated { true }
   end
+
+  factory :not-activated-user do
+    name { 'Test User' }
+    email { 'not_activated@example.com' }
+    password { 'password1' }
+    password_confirmation { 'password1' }
+    activated { false }
+  end
 end
